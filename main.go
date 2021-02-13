8ea7fec2658249e3a187c201952676413830ef6c
Luis Carlos <luiscarlosfirminopinheiro@gmail.com>
2021-02-13 17:39:52 -0300

    added basic server

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
+func main() {
+	http.HandleFunc("/", helloDocker)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func helloDocker(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello from Docker")
 }
